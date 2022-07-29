50d21a5d58da4f41d07fbc6cd8e08f26b44bcbf2
dennis mbithi <denmbithi@gmail.com>
2022-07-29 12:49:20 +0300

    authort initial files

---
+++ author/views.py
@@ -0,0 +1,8 @@
+from crypt import methods
+from flask import Blueprint
+
+author_app = Blueprint('author_app', __name__)
+
+@author_app.route('/register', methods=['GET', 'POST'])
+def register():
+    return 'Author registration'
