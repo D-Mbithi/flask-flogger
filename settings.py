711a773074178a0bdcf97f93f505f0f537b2bf91
dennis mbithi <denmbithi@gmail.com>
2022-07-29 12:46:47 +0300

    Update settings file

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ DB_USERNAME = os.environ['DB_USERNAME']
 DB_PASSWORD = os.environ['DB_PASSWORD']
 DB_HOST = os.environ['DB_HOST']
 DB_PORT = 5432
-DB_NAME = os.environ['DB_NAME']
+DB_NAME = os.environ['DATABASE_NAME']
 
 # DB Connection Config
 DB_URI = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
@@ -16,4 +16,4 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Flask settings
 BLOG_NAME = os.environ['BLOG_NAME']
-BLOG_IMAGE_PATH = os.environ['BLOG_IMAGE_PATH']
+BLOG_POST_IMAGES_PATH = os.environ['BLOG_POST_IMAGES_PATH']
