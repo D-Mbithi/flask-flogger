499ef60a51f0fffe74b79aba83e0c026f331bd44
dennis mbithi <denmbithi@gmail.com>
2022-07-29 12:26:14 +0300

    Applications settings

---
+++ settings.py
@@ -0,0 +1,19 @@
+import os
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# DB Conf settings
+DB_USERNAME = os.environ['DB_USERNAME']
+DB_PASSWORD = os.environ['DB_PASSWORD']
+DB_HOST = os.environ['DB_HOST']
+DB_PORT = 5432
+DB_NAME = os.environ['DB_NAME']
+
+# DB Connection Config
+DB_URI = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# Flask settings
+BLOG_NAME = os.environ['BLOG_NAME']
+BLOG_IMAGE_PATH = os.environ['BLOG_IMAGE_PATH']
